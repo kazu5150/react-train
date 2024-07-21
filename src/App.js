8e194251990ef1e32c5545713903df1edad12ec7
kazu5150 <kazu.deadtotheworld@gmail.com>
2024-07-21 19:03:00 +0900

    youtube 34:00

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { useState } from "react";
+import TodoList from "./TodoList";
+
 function App() {
-  return <div>Hello React on git by kazu</div>;
+  const [todos, setTodos] = useState([
+    {id:1, name: "Todo1", completed: false},
+  ]);
+
+  return (
+    <div>
+      <TodoList todos={todos} />
+      <input type="text"></input>
+      <button>タスクを追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク:0</div>
+    </div>
+  );
 }
 
 export default App;
